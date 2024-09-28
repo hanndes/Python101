d5f0c8ab7cd531ba7dd836b27693c692d4c1c11f
hande <handederelii@hotmail.com>
2024-09-28 15:39:45 +0300

    Change

--- p_main.py
+++ p_main.py
@@ -5,42 +5,25 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('Housing.csv')
 plt.figure()
-plt.hist(df['area'], bins=20, color='blue', edgecolor='white')
-plt.title('Area Distribution')
+plt.hist(df['area'], weights=df['price'],bins=20, color='pink', edgecolor='white')
 plt.xlabel('Area')
-plt.ylabel('Frequency')
+plt.ylabel('Price')
+plt.title('Ev Fiyatlarının Area ile İlişkisi')
 plt.show()
 
+
 plt.figure()
-plt.hist(df['bedrooms'], bins=20, color='green', edgecolor='white')
-plt.title('Bedrooms Distribution')
+plt.hist(df['bedrooms'], weights=df['price'], bins=10, color='lightblue', edgecolor='white')
 plt.xlabel('Bedrooms')
-plt.ylabel('Frequency')
+plt.ylabel('Total Price')
+plt.title('Ev Fiyatlarının Bedrooms ile İlişkisi')
 plt.show()
 
 plt.figure()
-plt.hist(df['stories'], bins=20, color='red', edgecolor='white')
-plt.title('Stories Distribution')
+plt.hist(df['stories'], weights=df['price'], bins=5, color='lightgreen', edgecolor='white')
 plt.xlabel('Stories')
-plt.ylabel('Frequency')
-plt.show()
-
-avg_price_by_bathrooms = df.groupby('bathrooms')['price'].mean()
-
-plt.figure()
-avg_price_by_bathrooms.plot(kind='bar', color='purple')
-plt.title('Average Price by Number of Bathrooms')
-plt.xlabel('Number of Bathrooms')
-plt.ylabel('Average Price')
-plt.show()
-
-avg_price_by_furnishing = df.groupby('furnishingstatus')['price'].mean()
-
-plt.figure()
-avg_price_by_furnishing.plot(kind='bar', color='orange')
-plt.title('Average Price by Furnishing Status')
-plt.xlabel('Furnishing Status')
-plt.ylabel('Average Price')
+plt.ylabel('Total Price')
+plt.title('Ev Fiyatlarının Stories ile İlişkisi')
 plt.show()
 
 
